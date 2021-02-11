ab01491b44968dcd3f8ae8881063558e7d82198a
Ryan Vice <76831245+ryanjvice@users.noreply.github.com>
2021-02-11 16:49:55 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function handleClick() {
+    let clickCount = 0;
+
+    $('.js-click-counter').text(clickCounter);
+
+    $('.js-clicker').click(function(event) {
+        
+        clickCount += 1;
+
+        $('.js-click-counter').text(clickCount);
+    });
+}
+
+$(handleClicks);
